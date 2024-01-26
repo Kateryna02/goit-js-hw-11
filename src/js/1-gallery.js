3e9f88e1c17d271f439840263fb76e057d1eda1e
Kateryna02 <150830031+Kateryna02@users.noreply.github.com>
2024-01-26 20:11:42 +0100

    hw-11

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,98 +1,120 @@
 // Описаний в документації
 import SimpleLightbox from "simplelightbox";
 
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-const images = [
-    {
-        preview:
-            "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
-        original:
-            "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
-        description: "Hokkaido Flower",
-    },
-    {
-        preview:
-            "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-        original:
-            "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-        description: "Container Haulage Freight",
-    },
-    {
-        preview:
-            "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-        original:
-            "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-        description: "Aerial Beach View",
-    },
-    {
-        preview:
-            "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-        original:
-            "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-        description: "Flower Blooms",
-    },
-    {
-        preview:
-            "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-        original:
-            "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-        description: "Alpine Mountains",
-    },
-    {
-        preview:
-            "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-        original:
-            "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-        description: "Mountain Lake Sailing",
-    },
-    {
-        preview:
-            "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-        original:
-            "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-        description: "Alpine Spring Meadows",
-    },
-    {
-        preview:
-            "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-        original:
-            "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-        description: "Nature Landscape",
-    },
-    {
-        preview:
-            "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-        original:
-            "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-        description: "Lighthouse Coast Sea",
-    },
-];
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
 
 
 
 
-const galleryContainer = document.querySelector('.gallery');
 
-const galleryItem = ({ preview, original, description }) => `
-    <li class="gallery-item">
-      <a class="gallery-link" href="${original}">
-        <img class="gallery-image" src="${preview}" alt="${description}" data-test="${original}">
-      </a>
-    </li>`;
-const gallery = (images) => {
-    const galleryItemHtml = images.map(galleryItem).join('');
-    galleryContainer.innerHTML = galleryItemHtml;
-    return galleryContainer;
-};
-gallery(images);
 
-new SimpleLightbox('.gallery a', {
+const modal = new SimpleLightbox('.gallery a', {
     captionDelay: 250,
     captions: true,
     captionsData: "alt",
     captionPosition: 'bottom',
-    
+
 
 });
+
+const galleryContainer = document.querySelector('.gallery');
+
+
+
+const galleryItem = ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+    <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+            <img class="gallery-image" src="${webformatURL}" alt="${tags}" data-test="${largeImageURL}">
+        </a>
+        <div class="image-details">
+           
+            <div class="info-container">
+                <span class="info-label">Likes:</span>
+                <span class="info-value">${likes}</span>
+            </div>
+            <div class="info-container">
+                <span class="info-label">Views:</span>
+                <span class="info-value">${views}</span>
+            </div>
+            <div class="info-container">
+                <span class="info-label">Comments:</span>
+                <span class="info-value">${comments}</span>
+            </div>
+            <div class="info-container">
+                <span class="info-label">Downloads:</span>
+                <span class="info-value">${downloads}</span>
+            </div>
+        </div>
+    </li>`;
+const gallery = (hits)  => {
+    console.log(hits);
+    const galleryItemHtml = hits.map(galleryItem).join('');
+    galleryContainer.innerHTML = galleryItemHtml;
+    modal.refresh();
+   
+};
+
+
+const form = document.getElementById('form');
+const loader = document.querySelector('.loader');
+
+form.addEventListener('submit', onSubmit) 
+function onSubmit(e) {
+    
+    e.preventDefault();
+    loader.classList.remove('is-hidden');
+    galleryContainer.innerHTML = "";
+    const keyword = e.currentTarget.elements.search.value.trim();
+        if (keyword !== '') {
+            fetchImages(keyword);
+        } else {
+            
+          
+        }
+        
+    };
+
+function fetchImages(keyword) {
+    const key = '42039867-09e41a1320e593858871044dc';
+    const params = new URLSearchParams({
+        key: key,
+        q: keyword,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true'
+   })
+    const url = `https://pixabay.com/api/?${params}`;
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.hits);
+            if (data.hits.length > 0) {
+                gallery(data.hits);
+            } else {
+                iziToast.error({
+                    title: 'error',
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'topRight',
+                    messageColor: 'white',
+                    backgroundColor: '#EF4040',
+                    
+               })
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        }).finally(() => {
+            loader.classList.add('is-hidden');
+        });
+}
+
+
+
 
